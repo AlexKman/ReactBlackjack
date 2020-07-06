fddc9541eaa486088cff75c312aaca5e7a3de210
Desktopy12 <Keeley.lp@gmail.com>
2020-07-06 22:04:53 +0100

    initialized state for game won and game over states and added functionality to deal cards

--- reactblackjack/src/App.js
+++ reactblackjack/src/App.js
@@ -5,8 +5,20 @@ import CardDeck from "./deck/cardDeck.js";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    playerHand: [],
+    dealerHand: [],
+    gameWon: false,
+    gameOver: false
+  };
+
+  dealCards = () => {
+    let cardDeck = cardDeck.dealCards(2);
+
+    return cardDeck;
+  };
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <h1>{this.cardDeck}</h1>;
   }
 }
 
